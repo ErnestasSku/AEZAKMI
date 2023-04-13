85e58bb4b9ee1c910aece91c70898301ac37e1b7
ErnestasSku <ernestassku@gmail.com>
2023-04-13 23:02:06 +0300

    Initial commit

---
+++ src/main/java/com/vu/aezakmi/AezakmiApplication.java
@@ -0,0 +1,13 @@
+package com.vu.aezakmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AezakmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AezakmiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vu/aezakmi/AezakmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vu.aezakmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AezakmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
