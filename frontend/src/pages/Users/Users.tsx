1e473beef2784309a29202763105a38cac7a8d98
Audrius Savickas <umarasss@gmail.com>
2023-04-25 21:27:23 +0300

    add test component

---
+++ frontend/src/pages/Users/Users.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import logo from '../../logo.svg'
+
+interface User {
+    id: number;
+    email: string;
+    password: string;
+    username: string;
+    role_id: unknown;
+}
+
+export const Users: React.FC = () => {
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('/users');
+            const body = await response.json();
+            setUsers(body);
+        }
+        fetchData();
+    }, [])
+
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+                <h2>Users</h2>
+                {users.map(user =>
+                    <div key={user.id}>
+                        {user.username} ({user.email})
+                    </div>
+                )}
+            </div>
+        </header>
+    )
+}
