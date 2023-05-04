d40c18fc92037d5cd521dc905e678830ac7d85de
Kristijonas Trinkūnas <krtrinkunas@gmail.com>
2023-05-04 13:04:16 +0300

    change a bit of security

--- src/main/java/com/vu/aezakmi/config/SecurityConfig.java
+++ src/main/java/com/vu/aezakmi/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -33,5 +35,10 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // (3)
                 .httpBasic(Customizer.withDefaults()) // (4)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/vu/aezakmi/service/UserService.java
+++ src/main/java/com/vu/aezakmi/service/UserService.java
@@ -4,12 +4,15 @@ import com.vu.aezakmi.controller.dto.UserDto;
 import com.vu.aezakmi.model.User;
 import com.vu.aezakmi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
@@ -20,5 +23,17 @@ public class UserService {
     public List<UserDto> getUsers() {
         List<User> allUsers =  userRepository.findAll();
         return allUsers.stream().map(user -> new UserDto(user)).toList();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().getType().name())
+                .build();
     }
 }
