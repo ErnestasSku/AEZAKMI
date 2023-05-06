ba2c0a309982fb990160c338e1b891e24fec09bc
Audrius Savickas <umarasss@gmail.com>
2023-05-06 20:08:15 +0300

    add lint plugin

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,8 +7,19 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 };
