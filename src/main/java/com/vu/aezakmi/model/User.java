f49cdd6a96c172eb142741530d5af016b9cdd431
Kristijonas Trinkūnas <krtrinkunas@gmail.com>
2023-05-08 11:04:44 +0300

    redo database prepopulation on startup

--- src/main/java/com/vu/aezakmi/model/User.java
+++ src/main/java/com/vu/aezakmi/model/User.java
@@ -17,13 +17,13 @@ public class User implements UserDetails {
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @ManyToOne(fetch = FetchType.LAZY)
