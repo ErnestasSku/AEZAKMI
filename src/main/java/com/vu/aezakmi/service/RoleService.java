3473083f501345d5a901c38e8873e966302723fb
Kristijonas Trinkūnas <krtrinkunas@gmail.com>
2023-05-08 11:04:44 +0300

    redo database prepopulation on startup

--- src/main/java/com/vu/aezakmi/service/RoleService.java
+++ src/main/java/com/vu/aezakmi/service/RoleService.java
@@ -16,24 +16,5 @@ public class RoleService {
         this.roleRepository = roleRepository;
     }
 
-    @PostConstruct
-    public void init() {
-        if (roleRepository.findByType(RoleType.ADMIN).isEmpty()) {
-            Role adminRole = new Role();
-            adminRole.setType(RoleType.ADMIN);
-            roleRepository.save(adminRole);
-        }
 
-        if (roleRepository.findByType(RoleType.TEACHER).isEmpty()) {
-            Role teacherRole = new Role();
-            teacherRole.setType(RoleType.TEACHER);
-            roleRepository.save(teacherRole);
-        }
-
-        if (roleRepository.findByType(RoleType.USER).isEmpty()) {
-            Role userRole = new Role();
-            userRole.setType(RoleType.USER);
-            roleRepository.save(userRole);
-        }
-    }
 }
