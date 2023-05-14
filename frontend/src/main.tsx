9f375469aebbc3e05bd754d1d592519dfccb6e90
Audrius Savickas <umarasss@gmail.com>
2023-05-14 21:25:45 +0300

    refactor video view

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,9 @@ import './index.css';
 import { AuthProvider } from './context/context.tsx';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- frontend/src/pages/Videos/VideoView.tsx
+++ frontend/src/pages/Videos/VideoView.tsx
@@ -1,32 +1,38 @@
 import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
-import { fetchVideo } from '../../api';
-import { useCallback } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { VideoPreview, fetchVideoBlob } from '../../api';
+import { useEffect, useRef, useState } from 'react';
 import { withPrivateRoute } from '../../components/PrivateRoute';
 
 export const VideoView = withPrivateRoute(() => {
   const { id = '' } = useParams<{ id: string }>();
-  const { data, isLoading } = useQuery('video', () => fetchVideo(id));
+  const { data: blob } = useQuery(`video-blob-${id}`, () => fetchVideoBlob(id));
+  const [videoUrl, setVideoUrl] = useState('');
+  const {
+    state: { video },
+  }: { state: { video: VideoPreview } } = useLocation();
+  const ref = useRef<HTMLVideoElement>(null);
 
-  const callbackRef = useCallback((node: HTMLVideoElement) => {
-    if (node !== null) {
-      node.play();
+  useEffect(() => {
+    if (blob?.data) {
+      setVideoUrl(URL.createObjectURL(blob.data));
     }
-  }, []);
+  }, [blob?.data]);
 
-  return isLoading ? (
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.play();
+    }
+  }, [videoUrl]);
+
+  return !videoUrl ? (
     <div>Loading...</div>
   ) : (
     <div style={{ marginTop: '24px' }}>
-      <video
-        controls={true}
-        ref={callbackRef}
-        width={1000}
-        src={`data:video/mp4;base64,${data?.data.data}`}
-      />
-      <h3>Title: {data?.data.title}</h3>
+      <video controls={true} ref={ref} width={1000} src={videoUrl} />
+      <h3>Title: {video.title}</h3>
       <h4>Description:</h4>
-      <p>{data?.data.description}</p>
+      <p>{video.description}</p>
     </div>
   );
 });
