f74938e1f9ed7af4df4e5c57953119c868c77361
Audrius Savickas <umarasss@gmail.com>
2023-05-15 18:44:44 +0300

    fix courses, videos BE api path

--- src/main/java/com/vu/aezakmi/controller/CourseController.java
+++ src/main/java/com/vu/aezakmi/controller/CourseController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/api/courses")
 public class CourseController {
 
     @Autowired
