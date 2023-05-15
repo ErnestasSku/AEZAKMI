3957380e1ab9078a353b6e1e0b5eaad7470cebeb
Kristijonas Trinkūnas <krtrinkunas@gmail.com>
2023-05-15 19:23:54 +0300

    extract user entity from JWT token and validate

--- src/main/java/com/vu/aezakmi/controller/CourseController.java
+++ src/main/java/com/vu/aezakmi/controller/CourseController.java
@@ -2,16 +2,21 @@ package com.vu.aezakmi.controller;
 
 import com.vu.aezakmi.dto.CourseDTO;
 import com.vu.aezakmi.model.Course;
+import com.vu.aezakmi.model.User;
 import com.vu.aezakmi.model.Video;
 import com.vu.aezakmi.service.CourseService;
+import com.vu.aezakmi.service.TokenService;
+import com.vu.aezakmi.service.UserService;
 import com.vu.aezakmi.service.VideoService;
 import com.vu.aezakmi.utils.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("api/courses")
@@ -23,8 +28,21 @@ public class CourseController {
     @Autowired
     private VideoService videoService;
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserService userService;
+
     @PostMapping
-    public ResponseEntity<?> createCourse(@RequestBody CourseDTO courseDto) {
+    @PreAuthorize("hasAnyRole('ADMIN', 'TEACHER')")
+    public ResponseEntity<?> createCourse(@RequestHeader("Authorization") String authorizationHeader, @RequestBody CourseDTO courseDto) {
+        String token = authorizationHeader.substring("Bearer ".length());
+        String username = tokenService.getUsernameFromToken(token);
+        User user = userService.getUserByUsername(username);
+        if(!Objects.equals(user.getId(), courseDto.getCreatorId())) {
+            return new ResponseEntity<>("user.getId() != courseDto.getCreatorId()", HttpStatus.FORBIDDEN);
+        }
         return courseService.create(courseDto);
     }
 
