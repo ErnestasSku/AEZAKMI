2bede57929046270e4c2a1e48bd57036e396d762
Audrius Savickas <umarasss@gmail.com>
2023-05-20 22:19:22 +0300

    show course name or creator name in Videos page

--- frontend/src/pages/Videos/Videos.tsx
+++ frontend/src/pages/Videos/Videos.tsx
@@ -1,20 +1,24 @@
 import { Typography } from '@mui/material';
 import { VideosList } from './VideosList';
 import { withPrivateRoute } from '../../components/PrivateRoute';
-import { useSearchParams } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import { Course } from '../../api';
 
 const Videos = () => {
   const [searchParams] = useSearchParams();
   const courseId = searchParams.get('courseId') ?? undefined;
   const creatorId = searchParams.get('creatorId') ?? undefined;
+  const {
+    state: { course },
+  }: { state: { course: Course } } = useLocation();
 
   return (
     <>
       <Typography sx={{ padding: '20px' }} variant="h4">
         {courseId
-          ? `All videos in Course ${courseId}`
+          ? `All videos in ${course.name}`
           : creatorId
-          ? `All videos by Creator ${creatorId}`
+          ? `All videos by ${course.creator.username}`
           : 'All videos'}
       </Typography>
       <div>
