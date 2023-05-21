ac42ad97dbb01e339ca455d3f6b410dc20482fcf
ErnestasSku <ernestassku@gmail.com>
2023-05-21 20:47:44 +0300

    Add permissions to logger

--- src/main/java/com/vu/aezakmi/interceptors/LoggingInterceptor.java
+++ src/main/java/com/vu/aezakmi/interceptors/LoggingInterceptor.java
@@ -1,11 +1,15 @@
 package com.vu.aezakmi.interceptors;
 
 
+import com.nimbusds.jose.proc.SecurityContext;
 import com.vu.aezakmi.config.ConfigurationRefresher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -14,8 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Component
 public class LoggingInterceptor implements HandlerInterceptor {
@@ -37,13 +43,20 @@ public class LoggingInterceptor implements HandlerInterceptor {
                     request.getUserPrincipal().getName() :
                     "anonymous";
 
-            //TODO: somehow add permissions
+            String permissions = "N/A";
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                permissions = userDetails.getAuthorities().stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining(", "));
+            }
 
             String className = handler.getClass().getName();
             String methodName = ((HandlerMethod)handler).getMethod().getName();
 
-            String logMessage = String.format("Timestamp: %s | Request URL: %s | Method: %s | Class Name: %s | Method Name: %s | User: %s",
-                    timestamp, requestURL, method, className, methodName, user);
+            String logMessage = String.format("Timestamp: %s | Request URL: %s | Method: %s | Class Name: %s | Method Name: %s | User: %s | Permissions: %s",
+                    timestamp, requestURL, method, className, methodName, user, permissions);
 
             System.out.println(logMessage);
             writeLogToFile(logMessage);
@@ -59,7 +72,4 @@ public class LoggingInterceptor implements HandlerInterceptor {
         }
     }
 
-    private boolean isLoggingEnabled() {
-        return true;
-    }
 }
