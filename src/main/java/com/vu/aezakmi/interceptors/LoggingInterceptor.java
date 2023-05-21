582bd934d6f6ac94989cbfa30fadc466337024e5
ErnestasSku <ernestassku@gmail.com>
2023-05-22 00:18:05 +0300

    Add logger strategies

--- src/main/java/com/vu/aezakmi/interceptors/LoggingInterceptor.java
+++ src/main/java/com/vu/aezakmi/interceptors/LoggingInterceptor.java
@@ -3,10 +3,13 @@ package com.vu.aezakmi.interceptors;
 
 import com.nimbusds.jose.proc.SecurityContext;
 import com.vu.aezakmi.config.ConfigurationRefresher;
+import com.vu.aezakmi.config.LoggingConfig;
+import com.vu.aezakmi.utils.LoggerStrategy;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,6 +24,7 @@ import java.io.PrintWriter;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -28,48 +32,21 @@ public class LoggingInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
     private final ConfigurationRefresher configurationRefresher;
 
-    public LoggingInterceptor(ConfigurationRefresher configurationRefresher) {
+    private LoggerStrategy loggerStrategy;
+
+
+    public LoggingInterceptor(ConfigurationRefresher configurationRefresher, LoggingConfig loggingConfig) {
         this.configurationRefresher = configurationRefresher;
+        this.loggerStrategy = loggingConfig.getLoggerStrategy();
+
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (configurationRefresher.isLoggingInterceptorEnabled()) {
-
-            LocalDateTime timestamp = LocalDateTime.now();
-            String requestURL = request.getRequestURL().toString();
-            String method = request.getMethod();
-            String user = request.getUserPrincipal() != null ?
-                    request.getUserPrincipal().getName() :
-                    "anonymous";
-
-            String permissions = "N/A";
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-                UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-                permissions = userDetails.getAuthorities().stream()
-                        .map(Object::toString)
-                        .collect(Collectors.joining(", "));
-            }
-
-            String className = handler.getClass().getName();
-            String methodName = ((HandlerMethod)handler).getMethod().getName();
-
-            String logMessage = String.format("Timestamp: %s | Request URL: %s | Method: %s | Class Name: %s | Method Name: %s | User: %s | Permissions: %s",
-                    timestamp, requestURL, method, className, methodName, user, permissions);
-
-            System.out.println(logMessage);
-            writeLogToFile(logMessage);
-        }
+        Optional<String> log = loggerStrategy.createLogMessage(request, response, handler, configurationRefresher);
+        loggerStrategy.writeToFile(log);
         return true;
     }
 
-    private void writeLogToFile(String logMessage) {
-        try (PrintWriter writer = new PrintWriter(new FileWriter("logs.txt", true))) {
-            writer.println(logMessage);
-        } catch (IOException e) {
-            logger.error("Failed to write log to file", e);
-        }
-    }
 
 }
