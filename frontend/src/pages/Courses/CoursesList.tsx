083c0a4824aaa69af89b535bce7c82ad075eaf65
Audrius Savickas <umarasss@gmail.com>
2023-05-22 18:22:17 +0300

    convert Courses/Videos List to use generic

--- frontend/src/pages/Courses/CoursesList.tsx
+++ frontend/src/pages/Courses/CoursesList.tsx
@@ -1,9 +1,9 @@
 import { useQuery } from 'react-query';
-import { fetchAllCourses } from '../../api';
+import { Course, fetchAllCourses } from '../../api';
 import { CoursePreview } from './CoursePreview';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDebounce } from '../../hooks/useDebounce';
-import { CircularProgress, Stack, TextField, Typography } from '@mui/material';
+import { SearchList } from '../../components/SearchList';
 
 export const CoursesList = () => {
   const [search, setSearch] = useState('');
@@ -12,43 +12,61 @@ export const CoursesList = () => {
     fetchAllCourses(debouncedSearch)
   );
 
-  return (
-    <Stack alignItems={'center'} sx={{ paddingX: '40px' }}>
-      <TextField
-        fullWidth
-        sx={{ marginY: '20px', maxWidth: '500px' }}
-        value={search}
-        label="Search courses"
-        placeholder='e.g. "Introduction to Programming in Java"'
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setSearch(event.target.value)
-        }
-      />
-      {isLoading ? (
-        <CircularProgress />
-      ) : (
-        <Stack gap={2} width={'100%'}>
-          <Typography textAlign={'left'} variant="h5">
-            {getResultsString(debouncedSearch, data?.data.length)}
-          </Typography>
-          <Stack gap={4}>
-            {data?.data.length === 0 && (
-              <Typography variant="h5">No results :/</Typography>
-            )}
-            {data?.data.map(course => (
-              <CoursePreview key={course.id} course={course} />
-            ))}
-          </Stack>
-        </Stack>
-      )}
-    </Stack>
+  const renderCoursePreview = (course: Course) => (
+    <CoursePreview key={course.id} course={course} />
   );
+
+  return (
+    <SearchList
+      isLoading={isLoading}
+      data={data?.data}
+      renderDataItem={renderCoursePreview}
+      setSearch={setSearch}
+      search={search}
+      debouncedSearch={debouncedSearch}
+      searchProps={{
+        label: 'Search courses',
+        placeholder: 'e.g. "Introduction to Programming in Java"',
+      }}
+      getResultsString={getResultsString}
+    />
+  );
+  // return (
+  //   <Stack alignItems={'center'} sx={{ paddingX: '40px' }}>
+  //     <TextField
+  //       fullWidth
+  //       sx={{ marginY: '20px', maxWidth: '500px' }}
+  //       value={search}
+  //       label="Search courses"
+  //       placeholder='e.g. "Introduction to Programming in Java"'
+  //       onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+  //         setSearch(event.target.value)
+  //       }
+  //     />
+  //     {isLoading ? (
+  //       <CircularProgress />
+  //     ) : (
+  //       <Stack gap={2} width={'100%'}>
+  //         <Typography textAlign={'left'} variant="h5">
+  //           {getResultsString(debouncedSearch, data?.data.length)}
+  //         </Typography>
+  //         <Stack gap={4}>
+  //           {data?.data.length === 0 && (
+  //             <Typography variant="h5">No results :/</Typography>
+  //           )}
+  //           {data?.data.map(course => (
+  //             <CoursePreview key={course.id} course={course} />
+  //           ))}
+  //         </Stack>
+  //       </Stack>
+  //     )}
+  //   </Stack>
+  // );
 };
 
 const getResultsString = (search?: string, size?: number) => {
   if (search) {
     return `"${search}" courses (${size}):`;
   }
-
   return `All courses (${size}):`;
 };
--- frontend/src/pages/Videos/VideosList.tsx
+++ frontend/src/pages/Videos/VideosList.tsx
@@ -3,6 +3,9 @@ import { VideoPreview, fetchAllVideoPreviews } from '../../api';
 import { useQuery } from 'react-query';
 import { convertArrayToChunks } from '../../utils/array';
 import { Grid } from '@mui/material';
+import { useState } from 'react';
+import { useDebounce } from '../../hooks/useDebounce';
+import { SearchList } from '../../components/SearchList';
 
 interface Props {
   courseId?: string;
@@ -10,40 +13,76 @@ interface Props {
 }
 
 export const VideosList = ({ courseId, creatorId }: Props) => {
+  const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search, 500);
   const { data, isLoading } = useQuery(
-    `videos-${JSON.stringify({ courseId, creatorId })}`,
+    ['videos', courseId, creatorId, debouncedSearch],
     () =>
       fetchAllVideoPreviews({
         courseId,
         creatorId,
+        search: debouncedSearch,
       })
   );
 
-  const chunks: VideoPreview[][] = convertArrayToChunks(data?.data, 3);
-
-  return isLoading ? (
-    <>
-      <div>Loading...</div>
-    </>
-  ) : !chunks[0]?.length ? (
-    <div>
-      {courseId
-        ? 'No videos added to course yet :('
-        : creatorId
-        ? 'No videos added by creator yet :('
-        : 'No videos added yet :('}{' '}
-    </div>
-  ) : (
-    <Grid container rowGap={6} padding={'20px 40px'}>
-      {chunks.map((chunk, index) => (
-        <Grid container key={index} wrap="nowrap" spacing={6}>
-          {chunk.map(video => (
-            <Grid key={video.id} item xs={4}>
-              <VideoPreviewView video={video} />
-            </Grid>
-          ))}
+  const renderVideoPreviewGrid = (chunk: VideoPreview[], index: number) => (
+    <Grid container key={index} wrap="nowrap" spacing={6}>
+      {chunk.map(video => (
+        <Grid key={video.id} item xs={4}>
+          <VideoPreviewView video={video} />
         </Grid>
       ))}
     </Grid>
   );
+
+  const chunks: VideoPreview[][] = convertArrayToChunks(data?.data, 3);
+
+  return (
+    <SearchList
+      data={chunks}
+      isLoading={isLoading}
+      renderDataItem={renderVideoPreviewGrid}
+      setSearch={setSearch}
+      search={search}
+      debouncedSearch={debouncedSearch}
+      searchProps={{
+        label: 'Search videos',
+        placeholder: 'e.g. "How to create a variable in Java?"',
+      }}
+      getResultsString={getResultsString}
+    />
+  );
+
+  // return isLoading ? (
+  //   <>
+  //     <div>Loading...</div>
+  //   </>
+  // ) : !chunks[0]?.length ? (
+  //   <div>
+  //     {courseId
+  //       ? 'No videos added to course yet :('
+  //       : creatorId
+  //       ? 'No videos added by creator yet :('
+  //       : 'No videos added yet :('}{' '}
+  //   </div>
+  // ) : (
+  //   <Grid container rowGap={6} padding={'20px 40px'}>
+  //     {chunks.map((chunk, index) => (
+  //       <Grid container key={index} wrap="nowrap" spacing={6}>
+  //         {chunk.map(video => (
+  //           <Grid key={video.id} item xs={4}>
+  //             <VideoPreviewView video={video} />
+  //           </Grid>
+  //         ))}
+  //       </Grid>
+  //     ))}
+  //   </Grid>
+  // );
+};
+
+const getResultsString = (search?: string, size?: number) => {
+  if (search) {
+    return `"${search}" videos (${size}):`;
+  }
+  return `All videos (${size}):`;
 };
