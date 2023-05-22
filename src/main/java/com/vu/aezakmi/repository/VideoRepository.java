9c3b3f389141058e5780e63dfa7ed315f13088e5
Audrius Savickas <umarasss@gmail.com>
2023-05-22 18:40:52 +0300

    use predicates for getAllVideos endpoint

--- src/main/java/com/vu/aezakmi/repository/VideoRepository.java
+++ src/main/java/com/vu/aezakmi/repository/VideoRepository.java
@@ -2,20 +2,11 @@ package com.vu.aezakmi.repository;
 
 import com.vu.aezakmi.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Collection;
 import java.util.List;
 
-public interface VideoRepository extends JpaRepository<Video, Long> {
-    @Query("SELECT v FROM Video v WHERE v.course.id = :courseId")
-    List<Video> findAllByCourseId(@Param("courseId") Long courseId);
-
-    @Query("SELECT v FROM Video v WHERE v.user.id = :creatorId")
-    List<Video> findAllByCreatorId(@Param("creatorId") Long creatorId);
-
-    List<Video> findByTitleContainingIgnoreCase(String title);
-
+public interface VideoRepository extends JpaRepository<Video, Long>, JpaSpecificationExecutor<Video> {
     List<Video> findByIdIn(Collection<Long> videoIds);
 }
