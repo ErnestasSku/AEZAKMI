b767dd7e5837e56d472dcbcbe12a05ff9743b7c9
Audrius Savickas <umarasss@gmail.com>
2023-05-23 18:22:14 +0300

    remove remaining unused page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import Users from './pages/Users';
 import Layout from './pages/Layout';
 import NoPage from './pages/NoPage';
 import Courses from './pages/Courses';
@@ -17,7 +16,6 @@ function App() {
       <Layout />
       <Routes>
         <Route path="" element={<Navigate to="/courses" />} />
-        <Route path="users" element={<Users />} />
         <Route path="courses" element={<Courses />} />
         <Route path="courses/create" element={<CreateCourse />} />
         <Route path="login" element={<Login />} />
